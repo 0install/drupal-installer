eaaab92153ee086428f290ff2dd466a7ff170106
Thomas Leonard <talex5@gmail.com>
2011-11-13 13:36:05 +0000

    Added support for modules

--- update-or-install-drupal.py
+++ update-or-install-drupal.py
@@ -47,14 +47,18 @@ def ensure_dir(path):
 			print ex
 			sys.exit(1)
 
-def ensure_link(target, symlink):
+def ensure_link(impl, target, symlink):
 	if os.path.islink(symlink):
 		old_target = os.readlink(symlink)
 		if old_target == target:
 			return
 		#print "rm " + symlink
 		os.unlink(symlink)
-	print "ln -s .../{name} {symlink}".format(name = os.path.basename(target), symlink = symlink)
+	print "ln -s <{component}-{version}>/{name} {symlink}".format(
+			component = os.path.basename(impl.feed),
+			version = impl.version,
+			name = os.path.basename(target),
+			symlink = symlink)
 	os.symlink(target, symlink)
 
 def ensure_owner(path, user):
@@ -69,13 +73,18 @@ def cp(source, target):
 	print "cp .../{name} {target}".format(name = os.path.basename(source), target = target)
 	shutil.copyfile(source, target)
 
+def rm(path):
+	print "rm", path
+	assert os.path.islink(path), path
+	os.unlink(path)
+
 ensure_dir(doc_root)
 
 for name in os.listdir(drupal_root):
 	full_name = os.path.join(drupal_root, name)
 	if name.endswith('.php') or (name != 'sites' and os.path.isdir(full_name)):
 		symlink = os.path.join(doc_root, name)
-		ensure_link(full_name, symlink)
+		ensure_link(drupal_impl, full_name, symlink)
 
 sites_dir = os.path.join(doc_root, 'sites')
 sites_default_dir = os.path.join(sites_dir, 'default')
@@ -84,7 +93,8 @@ ensure_dir(sites_default_dir)
 files_dir = os.path.join(sites_default_dir, 'files')
 ensure_dir(files_dir)
 
-ensure_link(os.path.join(drupal_root, 'sites', 'default', 'default.settings.php'),
+ensure_link(drupal_impl, 
+	    os.path.join(drupal_root, 'sites', 'default', 'default.settings.php'),
 	    os.path.join(doc_root, 'sites', 'default', 'default.settings.php'))
 
 htaccess = os.path.join(doc_root, '.htaccess')
@@ -97,5 +107,36 @@ if not os.path.exists(settings_file):
 
 ensure_owner(files_dir, apache_user)
 ensure_owner(settings_file, apache_user)
+
+print "Updating modules..."
+
+modules = {}
+site_modules_dir = os.path.join(sites_dir, 'all', 'modules')
+ensure_dir(site_modules_dir)
+
+old_modules = set()
+for module in os.listdir(site_modules_dir):
+	full_path = os.path.join(site_modules_dir, module)
+	if os.path.islink(full_path):
+		old_modules.add(module)
+
+for impl in selections.values():
+	for dep in impl.dependencies:
+		dep_impl = selections[dep.interface]
+		for binding in dep.bindings:
+			if isinstance(binding, model.EnvironmentBinding) and binding.name == 'drupal-modules':
+				name = os.path.basename(binding.insert)
+				impl_root = dep_impl.local_path or config.stores.lookup_any(dep_impl.digests)
+				target = os.path.join(impl_root, binding.insert)
+				if not os.path.exists(target):
+					print "Module root {target} does not exist (in {interface} {version})".format(
+							target = target,
+							interface = dep.interface,
+							version = dep_impl.version)
+				ensure_link(dep_impl, target, os.path.join(site_modules_dir, name))
+				if name in old_modules: old_modules.remove(name)
+
+for module in old_modules:
+	rm(os.path.join(site_modules_dir, module))
 
 print "Done"
